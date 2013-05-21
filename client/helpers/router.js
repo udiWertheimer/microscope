a0b6126a080e176f8ccab96cb7d07e0a562326dc
Udi Wertheimer <udi.wertheimer@gmail.com>
2013-05-21 20:25:20 +0300

    Denied access to new posts when not logged in.

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -5,4 +5,13 @@ Meteor.Router.add({
     and: function(id) { Session.set('currentPostId', id); }
   },
   '/submit': 'postSubmit'
-})
+});
+Meteor.Router.filters({
+  'requireLogin': function(page) {
+    if (Meteor.user())
+      return page;
+    else
+      return 'accessDenied';
+  }
+});
+Meteor.Router.filter('requireLogin', {only: 'postSubmit'});
